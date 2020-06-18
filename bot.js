ffb243bee7bfe0a2b586b2daf214d792ae718108
Neve0 <nevenarcisista@gmail.com>
2020-06-18 03:19:17 -0300

    added .env command

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 'user strict';
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 const prefix = '!'
+require('dotenv/config');
 
 client.on('ready', () => {
     console.log('Online');
@@ -15,4 +15,4 @@ client.on('message', message => {
     }
 });
 
-client.login(config.token)
+client.login(process.env.TOKEN)
